ddf335499209b67cc737b27c57ed64d457a4d961
nuttachai <b5576722_new@hotmail.com>
2015-08-11 17:01:29 +0700

    Servlet

---
+++ Servlet/src/com/Register.java
@@ -0,0 +1,23 @@
+package com;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Register extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest a, HttpServletResponse b) throws ServletException, IOException {
+		String name=a.getParameter("name");
+		System.out.println(name);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		}
+
+}
